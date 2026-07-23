bb627d9461bf554068f1b42c955b5b8aa9668d19
hjnj99999-blip <hjnj99999@gmail.com>
2026-07-23 17:08:54 +0800

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // 启动时动态申请麦克风权限
+  // 启动 App 时自动请求麦克风权限
   await Permission.microphone.request();
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
@@ -20,35 +20,22 @@ class MeetingWebApp extends StatefulWidget {
 }
 
 class _MeetingWebAppState extends State<MeetingWebApp> {
-  InAppWebViewController? webViewController;
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..loadRequest(Uri.parse('https://xzscl.duckdns.org'));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // 沉浸式体验，不留顶部边框，或者保留标题栏
       body: SafeArea(
-        child: InAppWebView(
-          initialUrlRequest: URLRequest(
-            url: WebUri("https://xzscl.duckdns.org"),
-          ),
-          initialSettings: InAppWebViewSettings(
-            // 允许网页自动播放声音和使用麦克风
-            mediaPlaybackRequiresUserGesture: false,
-            allowsInlineMediaPlayback: true,
-            // 允许 H5 权限请求
-            useOnPermissionRequest: true,
-          ),
-          onWebViewCreated: (controller) {
-            webViewController = controller;
-          },
-          // 自动同意网页发起的麦克风权限申请
-          androidOnPermissionRequest: (controller, origin, resources) async {
-            return PermissionRequestResponse(
-              resources: resources,
-              action: PermissionRequestResponseAction.GRANT,
-            );
-          },
-        ),
+        child: WebViewWidget(controller: _controller),
       ),
     );
   }
