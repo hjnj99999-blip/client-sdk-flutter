7a20b29fb706c5491babad38a5ce4307cf6acb08
hjnj99999-blip <hjnj99999@gmail.com>
2026-07-23 17:41:37 +0800

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-// 引入 Android 专属控件以支持网页权限放行
-import 'package:webview_flutter_android/webview_flutter_android.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // 1. 动态申请安卓原生麦克风权限
+  // 1. 启动时申请安卓原生麦克风权限
   await Permission.microphone.request();
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
@@ -28,23 +26,13 @@ class _MeetingWebAppState extends State<MeetingWebApp> {
   void initState() {
     super.initState();
 
-    final WebViewController controller = WebViewController()
+    _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..setBackgroundColor(const Color(0x00000000))
+      ..setOnConsoleMessage((message) {
+        debugPrint('WebView Console: ${message.message}');
+      })
       ..loadRequest(Uri.parse('https://xzscl.duckdns.org'));
-
-    // 2. 关键代码：自动放行网页（H5）发起的麦克风/摄像头请求
-    if (controller.platform is AndroidWebViewController) {
-      AndroidWebViewController.enableDebugging(true);
-      (controller.platform as AndroidWebViewController)
-          .setOnPlatformPermissionRequest(
-        (AndroidWebViewPermissionRequest request) {
-          request.grant(); // 自动批准 H5 网页的麦克风请求
-        },
-      );
-    }
-
-    _controller = controller;
   }
 
   @override
